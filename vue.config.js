8330fddc627d895fb86a0005674a148d008f748a
Yuriy Bevov <jury19y@mail.ru>
2022-05-01 14:52:27 +0300

    path

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/beer-chooser/dist/'
+    : '/'
 })
+
